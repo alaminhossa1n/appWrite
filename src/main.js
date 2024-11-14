0c7b35d39a6c9e77551c6d8e5b951ff8ee23a208
unknown <alaminatghoria@gmail.com>
2024-11-14 09:50:15 +0600

    change main file

--- src/main.js
+++ src/main.js
@@ -1,31 +1,37 @@
-import { Client, Users } from 'node-appwrite';
+const sdk = require("node-appwrite");
 
-// This Appwrite function will be executed every time your function is triggered
-export default async ({ req, res, log, error }) => {
-  // You can use the Appwrite SDK to interact with other services
-  // For this example, we're using the Users service
-  const client = new Client()
-    .setEndpoint(process.env.APPWRITE_FUNCTION_API_ENDPOINT)
-    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-    .setKey(req.headers['x-appwrite-key'] ?? '');
-  const users = new Users(client);
+const client = new sdk.Client();
+const databases = new sdk.Databases(client);
 
-  try {
-    const response = await users.list();
-    // Log messages and errors to the Appwrite Console
-    // These logs won't be seen by your end users
-    log(`Total users: ${response.total}`);
-  } catch(err) {
-    error("Could not list users: " + err.message);
+// Set up Appwrite client with environment variables
+client
+  .setEndpoint(process.env.APPWRITE_ENDPOINT)
+  .setProject(process.env.APPWRITE_PROJECT_ID)
+  .setKey(process.env.APPWRITE_API_KEY);
+
+module.exports = async function (req, res) {
+  // Check the method and path for routing
+  if (req.path === "/" && req.method === "GET") {
+    return res.send("Hello World!");
   }
 
-  // The req object contains the request data
-  if (req.path === "/ping") {
-    // Use res object to respond with text(), json(), or binary()
-    // Don't forget to return a response!
-    return res.text("Pong");
+  if (req.path === "/add" && req.method === "POST") {
+    const { data } = req.body;
+
+    try {
+      // Create a document in Appwrite database
+      const response = await databases.createDocument(
+        process.env.APPWRITE_DATABASE_ID,
+        process.env.APPWRITE_COLLECTION_ID,
+        "unique()",
+        data
+      );
+      return res.status(200).json({ message: "Data added successfully", response });
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
   }
-  
+
   if (req.path === "/data" && req.method === "GET") {
     try {
       // Fetch documents from Appwrite database
@@ -39,10 +45,6 @@ export default async ({ req, res, log, error }) => {
     }
   }
 
-  return res.json({
-    motto: "Build like a team of hundreds_",
-    learn: "https://appwrite.io/docs",
-    connect: "https://appwrite.io/discord",
-    getInspired: "https://builtwith.appwrite.io",
-  });
+  // Return 404 for unsupported routes
+  return res.status(404).json({ error: "Route not found" });
 };
