d25b3cf293b42c8448f08f9bb26e2b304eef847e
unknown <alaminatghoria@gmail.com>
2024-11-14 10:18:42 +0600

    main file change

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { Client, Databases } from 'node-appwrite';
-require("dotenv").config();
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default async ({ req, res, log, error }) => {
   const client = new Client()
@@ -7,15 +9,14 @@ export default async ({ req, res, log, error }) => {
     .setProject(process.env.APPWRITE_PROJECT_ID)
     .setKey(process.env.APPWRITE_API_KEY);
 
-
   const databases = new Databases(client);
   // Route handling based on request path
-  if (req.path === "/ping") {
-    return res.text("Pong");
+  if (req.path === '/ping') {
+    return res.text('Pong');
   }
 
   // Fetch data from the Appwrite database
-  if (req.path === "/data" && req.method === "GET") {
+  if (req.path === '/data' && req.method === 'GET') {
     try {
       const response = await databases.listDocuments(
         process.env.APPWRITE_DATABASE_ID,
@@ -23,17 +24,17 @@ export default async ({ req, res, log, error }) => {
       );
       return res.json(response.documents);
     } catch (err) {
-      error("Could not retrieve data: " + err.message);
+      error('Could not retrieve data: ' + err.message);
       return res.status(500).json({ error: err.message });
     }
   }
 
   // Default response
   return res.json({
-    motto: "Build like a team of hundreds_",
-    learn: "https://appwrite.io/docs",
-    connect: "https://appwrite.io/discord",
-    getInspired: "https://builtwith.appwrite.io",
-    Message: "Hello World!"
+    motto: 'Build like a team of hundreds_',
+    learn: 'https://appwrite.io/docs',
+    connect: 'https://appwrite.io/discord',
+    getInspired: 'https://builtwith.appwrite.io',
+    Message: 'Hello World!',
   });
 };
